93fc0341a79ec1923714a57cd74aab85b5bb2a38
tjswinson <tiffanyjswinson@gmail.com>
2022-03-19 08:08:44 -0400

    add  css styling

--- react-ui/src/Components/views/forecastForm.jsx
+++ react-ui/src/Components/views/forecastForm.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 
 const ForecastForm = () => {
-    
-
+   
     const [zipCode, setZipCode] = useState('');
 
 const handleChange = e => {
@@ -10,31 +9,43 @@ const handleChange = e => {
     setZipCode(target);
   };
 
-  const handleSubmit = e => {
-    e.preventDefault();
-      
-  
-  
+    const handleSubmit = e => {
+        e.preventDefault();
+         
+    const CurrentForecast = () => {
+
+    const [currentWeather, setCurrentWeather] = useState('');
+
+           
+const GetData = () => {
     fetch(`http://localhost:8080/location/${zipCode}`)
     .then(data => data.json())
     .then(response => {
       console.log(response)
-    })
+    });
+    
   }
   
-
+  
+};
+    }
+    
     return (
+      <>
     <form onSubmit={handleSubmit}> 
       <div className="forecast">
         <label> Please enter zip code</label>
         <input onChange={handleChange} value = {zipCode} />
       </div>
-      <button>Submit</button>
-      <div className="weather">
-
-      </div>
-    </form>  
+      <button>Get Forecast</button>
+      
+    </form> 
+     
+    </>
+     
+    
   )
-};
+    }
+
 
 export default ForecastForm;
